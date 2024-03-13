9ea733e516bfb1258ac20e2269509f1f80e6a7a2
fredcallaway <fredc@llaway.com>
2024-03-13 15:33:43 -0400

    improve data viewer

--- static/js/demo.js
+++ static/js/demo.js
@@ -1,6 +1,6 @@
 // code for demonstration widgets, not part of the actual experiment
 
-
+solutionData = undefined
 
 async function puzzleViewer(name) {
   let showSolution = !!urlParams.showSolution
@@ -73,7 +73,6 @@ async function puzzleViewer(name) {
 
     }
 
-
   if (name && _.find(PUZZLES, {name})) {
     showPuzzle(_.find(PUZZLES, {name}))
   } else {
@@ -81,61 +80,17 @@ async function puzzleViewer(name) {
   }
 }
 
-async function altPuzzleViewer() {
-  let stimData = await $.getJSON(`static/json/stimuli-alt.json`)
-  let cv = new CycleViewer(DISPLAY, stimData, function(stims) {
-    let div = this.content
-    this.setTitle(`border = ${stims.target_border}`)
-
-    async function showPuzzle(trial) {
-      await new BlockPuzzle({...trial, allowQuitSeconds: 0, prompt: trial.name}).run(div)
-      showSelector()
-    }
-
-    function showSelector() {
-      div.empty()
-      // let basic = _.map(_.shuffle(), 'name')
-      // let compositions = _.map(_.shuffle(STIMULI.compositions), 'name')
-      let basicDiv = $('<div>').appendTo(div)
-      for (let stim of stims.basic) {
-        let div = $('<div>').css({
-          display: 'inline-block',
-          cursor: 'pointer',
-          width: '240px',
-        }).appendTo(basicDiv)
-        new BlockDisplayOnly({...stim, grid: 15}).attach(div)
-        div.click(() => showPuzzle(stim))
-      }
-
-      let compDiv = $('<div>').css('margin-top', 30).appendTo(div)
-      let rows = _.zip(..._.chunk(stims.compositions, stims.basic.length-1))
-      for (let row of rows) {
-        let rowDiv = $('<div>').css('margin-top', 10).appendTo(compDiv).css('border', 'thin black')
-        for (let stim of row) {
-          let div = $('<div>').css({
-            display: 'inline-block',
-            cursor: 'pointer',
-            width: '240px',
-          }).appendTo(rowDiv)
-          new BlockDisplayOnly({...stim, grid: 15}).attach(div)
-          div.click(() => showPuzzle(stim))
-        }
-      }
-    }
-    showSelector()
-  })
-  cv.showItem(0)
-}
-
+var _generation = 1
 async function genDataViewer() {
   DISPLAY.empty()
-  let allData = await $.getJSON(`static/json/solutions2.json`)
+  genData = _.values(_.groupBy(solutionData, 'generation'))
 
-  let cv = new CycleViewer(DISPLAY, allData, function(grp) {
+  let cv = new CycleViewer(DISPLAY, genData, function(data) {
     this.content.empty()
-    this.setTitle(`generation = ${grp.generation}`)
+    _generation = data[0].generation
+    this.setTitle(`generation = ${_generation}`)
 
-    for (let pdata of grp.data) {
+    for (let pdata of data) {
       let col = $('<div>').css({
         width: 180,
         height: 290,
@@ -143,10 +98,10 @@ async function genDataViewer() {
         border: 'medium black solid',
         margin: 5
       }).appendTo(this.content)
-      $('<p>').text(pdata.uid).css({'font-size': 12, 'font-weight': 'bold'}).appendTo(col)
+      $('<p>').text(pdata.pid).css({'font-size': 12, 'font-weight': 'bold'}).appendTo(col)
       col.click(() => {
         this.listener.clear()
-        dataViewer(pdata.uid)
+        dataViewer(pdata.pid)
       })
       for (let trial of pdata.solutions) {
         let div = $('<div>').appendTo(col)
@@ -155,99 +110,22 @@ async function genDataViewer() {
       }
     }
   })
-  cv.showItem(0)
+  cv.showItem(_generation-1)
 
 }
 
-async function dataViewer(which) {
-  let listen = new EventListeners()
+async function dataViewer(pid) {
+  console.log('dataViewer pid', pid)
 
   DISPLAY.empty()
-  DISPLAY.css('width', 1400)
-  let wrapper = $('<div>').css({
-    'position': 'relative',
-    'margin': 'auto',
-    'width': '1400px',
-    'text-align': 'center',
-    // 'border': 'thin red solid'
-  }).appendTo(DISPLAY)
 
-  $('<button>')
-  .appendTo(wrapper)
-  .css({
-    'position': 'absolute',
-    'left': '50px',
-    'top': '10px'
-  })
-  .addClass('btn')
-  .html('← generations')
-  .css('font-size', 18)
-  .click(() => {
-    listen.clear()
-    genDataViewer()
-  })
+  let cv = new CycleViewer(DISPLAY, solutionData, function(data) {
+    this.content.empty()
+    this.setTitle(data.pid)
+    _generation = data.generation
+    // queryParams.set("data", `${data.uid}`);
 
-  let title = $('<h1>').appendTo(wrapper)
-
-  let content = $('<div>').css({
-    // 'margin-left': '100px',
-    'width': '100%',
-    // border: 'thick black solid'
-  }).appendTo(wrapper)
-
-  let btnPrev = $('<button>')
-  .addClass('btn')
-  .text('<<')
-  .css({
-    position: 'absolute',
-    top: '10px',
-    left: '370px',
-  })
-  .appendTo(wrapper)
-
-
-  let btnNext = $('<button>')
-  .addClass('btn')
-  .text('>>')
-  .css({
-    position: 'absolute',
-    top: '10px',
-    right: '370px',
-  })
-  .appendTo(wrapper)
-
-  // let [version, uid] = which.split('-');
-  let allData = await $.getJSON(`static/json/solutions.json`)
-  makeGlobal({allData})
-  var queryParams = new URLSearchParams(window.location.search);
-
-  async function show(i) {
-    let data = allData[i]
-    queryParams.set("data", `${data.uid}`);
-    history.pushState(null, null, "?"+queryParams.toString());
-
-    btnPrev.unbind('click')
-    btnPrev.click(() => {
-      show(mod(i - 1, allData.length))
-    })
-    btnNext.unbind('click')
-    btnNext.click(() => {
-      show(mod(i + 1, allData.length))
-    })
-    listen.on('keydown', event => {
-      if (event.key === "ArrowRight") {
-        listen.clear()
-        show(mod(i + 1, allData.length))
-      }
-      else if (event.key === "ArrowLeft") {
-        listen.clear()
-        show(mod(i - 1, allData.length))
-      }
-    })
-
-    content.empty()
-    title.text(data.uid)
-    let row1 = $('<div>').appendTo(content)
+    let row1 = $('<div>').appendTo(this.content)
     $('<h1>').text("Examples").css('margin-top', '40px').appendTo(row1)
     for (let trial of data.examples) {
       let div = $('<div>').css('display', 'inline-block').appendTo(row1)
@@ -255,15 +133,31 @@ async function dataViewer(which) {
       new BlockDisplayOnly(trial).attach(div)
     }
 
-    let row2 = $('<div>').appendTo(content)
+    let row2 = $('<div>').appendTo(this.content)
     $('<h1>').text("Solutions").css('margin-top', '40px').appendTo(row2)
     for (let trial of data.solutions) {
       let div = $('<div>').css('display', 'inline-block').appendTo(row2)
       trial = {...trial, grid: 20}
       new BlockDisplayOnly(trial).attach(div)
     }
-  }
-  await show(Math.max(0,_.findIndex(allData, {uid: which})))
+  })
+
+  $('<button>')
+  .appendTo(cv.top)
+  .css({
+    'position': 'absolute',
+    'left': '50px',
+    'top': '10px'
+  })
+  .addClass('btn')
+  .html('← generation')
+  .css('font-size', 18)
+  .click(() => {
+    cv.listener.clear()
+    genDataViewer(_generation)
+  })
+
+  cv.showItem(Math.max(0, _.findIndex(solutionData, {pid})))
 }
 
 
@@ -290,10 +184,11 @@ async function handleSpecialMode() {
     await puzzleViewer(urlParams.puzzle)
   }
   else if (urlParams.data != undefined) {
-    if (urlParams.data == 'generations') {
-      await genDataViewer()
-    } else {
+    solutionData = await $.getJSON(`static/json/solutions.json`)
+    if (urlParams.data) {
         await dataViewer(urlParams.data)
+    } else {
+      await genDataViewer()
     }
   }
   else if (urlParams.dev != undefined) {
